e3d0c1ee533be65cca6bda9680d0f0b2b0306229
nishit <nishitmv@gmail.com>
2022-06-10 00:56:52 +0530

    K Closest Elements

--- src/main/java/com/nitron/heapalgos/KClosestElements/KclosestElementsFinder.java
+++ src/main/java/com/nitron/heapalgos/KClosestElements/KclosestElementsFinder.java
@@ -5,19 +5,19 @@ import java.util.*;
 public class KclosestElementsFinder {
     public List<Integer> find(int[] arr, int k, int x) {
 
-        var minHeap = new PriorityQueue<Tuple>( Comparator.comparingInt(Tuple::getDiff).reversed());
+        var maxHeap = new PriorityQueue<Tuple>( Comparator.comparingInt(Tuple::getDiff).reversed());
 
         for (int ele : arr) {
             int diff = 0;
             diff = ele > x ? ele - x : x - ele;
-            minHeap.add(new Tuple(ele, diff));
-            if (minHeap.size() > k)
-                minHeap.remove();
+            maxHeap.add(new Tuple(ele, diff));
+            if (maxHeap.size() > k)
+                maxHeap.remove();
         }
 
         var list = new ArrayList<Integer>();
 
-       minHeap.forEach(t -> list.add(t.getValue()));
+       maxHeap.forEach(t -> list.add(t.getValue()));
         return list;
     }
 }
